9d18299d77c3229b911665660572a8ae8a02853e
Werner Wessely <wessely.werner@gmail.com>
2022-06-09 12:13:15 +0200

    Add rust & cmake lsp to nvchad

--- nvchad/.config/nvim/lua/custom/plugins/lspconfig.lua
+++ nvchad/.config/nvim/lua/custom/plugins/lspconfig.lua
@@ -3,7 +3,7 @@ local M = {}
 M.setup_lsp = function(attach, capabilities)
    local lspconfig = require "lspconfig"
 
-   local servers = { "clangd", "gopls", "sumneko_lua" }
+   local servers = { "clangd", "gopls", "sumneko_lua", "cmake", "rust_analyzer" }
 
    for _, lsp in ipairs(servers) do
       lspconfig[lsp].setup {
