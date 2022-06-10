13637964964a34388d9a76c7a9c46a6a51d67a65
Werner Wessely <wessely.werner@gmail.com>
2022-06-10 12:41:46 +0200

    Format in nvchad

--- nvchad/.config/nvim/lua/custom/plugins/init.lua
+++ nvchad/.config/nvim/lua/custom/plugins/init.lua
@@ -1,32 +1,32 @@
 return {
+	-- add pounce
+	["rlane/pounce.nvim"] = {
+		config = function()
+			require("pounce").setup({
+				multi_window = true,
+			})
+		end,
+	},
 
-   -- add pounce
-   ["rlane/pounce.nvim"] = {
-      config = function()
-         require("pounce").setup {
-            multi_window = true,
-         }
-      end,
-   },
+	-- add trim
+	["cappyzawa/trim.nvim"] = {
+		config = function()
+			require("trim").setup({
+				disable = {}, -- ignore these files
+				patterns = {
+					[[%s/\s\+$//e]], -- remove unwanted spaces
+					[[%s/\($\n\s*\)\+\%$//]], -- trim last line
+					[[%s/\%^\n\+//]], -- trim first line
+				},
+			})
+		end,
+	},
 
-   -- add trim
-   ["cappyzawa/trim.nvim"] = {
-      config = function()
-         require("trim").setup {
-            disable = {}, -- ignore these files
-            patterns = {
-               [[%s/\s\+$//e]], -- remove unwanted spaces
-               [[%s/\($\n\s*\)\+\%$//]], -- trim last line
-               [[%s/\%^\n\+//]], -- trim first line
-            },
-         }
-      end,
-   },
-
-   ["jose-elias-alvarez/null-ls.nvim"] = {
-      after = "nvim-lspconfig",
-      config = function()
-         require "custom.plugins.null-ls"
-      end,
-   },
+	-- add null-ls
+	["jose-elias-alvarez/null-ls.nvim"] = {
+		after = "nvim-lspconfig",
+		config = function()
+			require("custom.plugins.null-ls")
+		end,
+	},
 }
