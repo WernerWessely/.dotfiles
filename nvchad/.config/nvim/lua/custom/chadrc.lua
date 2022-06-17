5e2a73b0fe41893d79bcbc3c345724ac959047c4
Werner Wessely <wessely.werner@gmail.com>
2022-06-17 21:48:53 +0200

    Add git status to nvim-tree in nvchad

--- nvchad/.config/nvim/lua/custom/chadrc.lua
+++ nvchad/.config/nvim/lua/custom/chadrc.lua
@@ -7,9 +7,9 @@ local M = {}
 
 -- load in options
 M.options = {
-   user = function()
-      require "custom.options"
-   end,
+    user = function()
+        require "custom.options"
+    end,
 }
 
 -- load in mappings
@@ -18,27 +18,44 @@ M.mappings = require "custom.mappings"
 -- load in plugins
 M.plugins = {
 
-   options = {
-      lspconfig = {
-         setup_lspconf = "custom.plugins.lspconfig",
-      },
+    options = {
+        lspconfig = {
+            setup_lspconf = "custom.plugins.lspconfig",
+        },
 
-      statusline = {
-         separator_style = "round",
-      },
-   },
+        statusline = {
+            separator_style = "round",
+        },
+    },
 
-   user = require "custom.plugins",
+    -- enable git in tree
+    override = {
+        ["kyazdani42/nvim-tree.lua"] = {
+            renderer = {
+                icons = {
+                    show = {
+                        git = true,
+                    },
+                },
+            },
+            git = {
+                enable = true,
+                ignore = false,
+            }
+        },
+    },
+
+    user = require "custom.plugins",
 }
 
 -- ui settings
 M.ui = {
-   -- set the nord theme
-   theme = "nord",
-   -- enable the cursor line
-   hl_override = {
-      CursorLine = { bg = "one_bg" },
-   },
+    -- set the nord theme
+    theme = "nord",
+    -- enable the cursor line
+    hl_override = {
+        CursorLine = { bg = "one_bg" },
+    },
 }
 
 return M
