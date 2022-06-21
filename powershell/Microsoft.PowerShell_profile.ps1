cf35b199bab9b3bb237671f6f7884787b497b328
Werner Wessely <wessely.werner@gmail.com>
2022-06-21 13:07:50 +0200

    Remove old z integration from powershell

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -110,16 +110,9 @@ function v {
     nvim '"+Telescope find_files hidden=true"' $args
 }
 
-# For zoxide v0.8.0+
 Invoke-Expression (& {
     $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
     (zoxide init --hook $hook powershell | Out-String)
-})
-
-# For older versions of zoxide
-Invoke-Expression (& {
-    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
-    (zoxide init --hook $hook powershell) -join "`n"
 })
 
 Invoke-Expression (&starship init powershell)
