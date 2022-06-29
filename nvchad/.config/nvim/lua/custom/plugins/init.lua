d1ccf245df5a7ef0b59881086689d13cc9ea1cf6
Werner Wessely <wessely.werner@gmail.com>
2022-06-29 08:58:54 +0200

    Do not trim last empty line in nvchad

--- nvchad/.config/nvim/lua/custom/plugins/init.lua
+++ nvchad/.config/nvim/lua/custom/plugins/init.lua
@@ -15,7 +15,7 @@ return {
 				disable = {}, -- ignore these files
 				patterns = {
 					[[%s/\s\+$//e]], -- remove unwanted spaces
-					[[%s/\($\n\s*\)\+\%$//]], -- trim last line
+					-- [[%s/\($\n\s*\)\+\%$//]], -- trim last line
 					[[%s/\%^\n\+//]], -- trim first line
 				},
 			})
